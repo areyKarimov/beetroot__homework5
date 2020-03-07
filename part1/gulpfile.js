a60fd0edfed5c1007b8dfb817ef95b465ee55682
Arey Karimov <areykarimov@gmail.com>
2020-03-07 23:24:24 +0200

    homework5

---
+++ part1/gulpfile.js
@@ -0,0 +1,15 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch');
+
+gulp.task('scss', function() {
+    return gulp.src('app/scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css/'))
+})
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/*.scss', gulp.parallel('scss'));
+})
+
+gulp.task('default', gulp.parallel('watch'));
